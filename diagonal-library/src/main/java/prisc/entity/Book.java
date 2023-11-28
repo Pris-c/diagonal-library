a2f0cc1cc8e08ed30691071542607fe8c3adebf0
Priscila <priscila.campoos@yahoo.com.br>
2023-11-28 10:42:43 +0000

    Setup Hibernate with PostgreSQL

---
+++ diagonal-library/src/main/java/prisc/entity/Book.java
@@ -0,0 +1,64 @@
+package prisc.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+//@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int bookId;
+    private String title;
+    private String author;
+    private int year;
+
+    public Book() {
+    }
+
+    public Book(int bookId, String title, String author, int year) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return " Book " + bookId + ", Title: " + title + ", Author: " + author + ", Year: " + year;
+    }
+}
---
+++ diagonal-library/src/main/java/prisc/util/HibernateUtil.java
@@ -0,0 +1,90 @@
+package prisc.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+
+/**
+ * Configures Hibernate in the application, creating methods to get the Sessions Factory and to turn off
+ * the register when the application is closed.
+ */
+public class HibernateUtil {
+
+    /* Storing and managing the configurations needed to run hibernate. */
+    private static StandardServiceRegistry registry;
+
+    /*
+    Created once during application startup and shared throughout the application's.
+    Provides efficient interaction between Java objects and the database.
+     */
+    private static SessionFactory sessionFactory;
+
+    /**
+     * Gets an instance of SessionFactory, Hibernate's main interface for creating sessions with the database.
+     * A session is a unit of work that encapsulates read and write operations on the database.
+     * It implements the singleton pattern concept,checking if a session has already been created.
+     */
+    public static SessionFactory getSessionFactory() {
+
+        if (sessionFactory == null) {
+            try {
+                /*
+                Standard Service Registry creation.
+                The configure() method gets the Hibernate specif setting, such as the database URL, access credentials
+                and other options from file hibernate.cfg.xml.
+                 */
+                registry = new StandardServiceRegistryBuilder().configure().build();
+
+                /*
+                Metadata Source creation
+                It aggregates and manages different sources or metadata, which can include JPA (Java Persistence API)
+                annotations, XML mapping files or Java classes annotated with mapping information.
+                This is where you tell Hibernate about your entities, their mappings and other settings.
+                 */
+                MetadataSources sources = new MetadataSources(registry);
+
+
+                /*
+                 Metadata creation
+                 The metadata represents the actual Hibernate metadata after it has been built from the sources.
+                 In Hibernate context, metadata refers to the detailed  information about the mapping between Java
+                  objects and database tables. Some of the mainly components of Hibernate metadata are: Class mapping,
+                  Annotations or XML Mapping Files, Relationship between classes, Primary and secondary keys and
+                  Cache configurations (if applicable).
+                 */
+                Metadata metadata = sources.getMetadataBuilder().build();
+
+                /*
+                 SessionFactory creation
+                 Responsible for creating Session instances, which are objects that provide a unit of
+                  work and act as a bridge between Java objects and database operations. Sessions encapsulates
+                  database operations.
+                 Furthermore, the Session Factory works transaction management (ensuring the atomicity and
+                 consistency of database operations) and connection pooling (optimizing performance by reusing
+                 database connections).
+                 */
+                sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                if (registry != null) {
+                    StandardServiceRegistryBuilder.destroy(registry);
+                }
+            }
+        }
+        return sessionFactory;
+    }
+
+    /**
+     Closes the Hibernate registry to release resources, ensure the proper completion of Hibernate operations, and
+     prevent resource leaks.
+     */
+    public static void shutdown() {
+        if (registry != null) {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
