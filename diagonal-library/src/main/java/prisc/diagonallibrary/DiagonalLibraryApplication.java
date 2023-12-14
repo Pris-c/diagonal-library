c5d2dbce53eeb8574c2a360fdc23d998263cb007
Priscila <priscila.campoos@yahoo.com.br>
2023-12-14 11:37:16 +0000

    Initial configuration of the Spring Boot project and integration to the PostgreSQL database

---
+++ diagonal-library/src/main/java/prisc/diagonallibrary/DiagonalLibraryApplication.java
@@ -0,0 +1,13 @@
+package prisc.diagonallibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagonalLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagonalLibraryApplication.class, args);
+	}
+
+}
