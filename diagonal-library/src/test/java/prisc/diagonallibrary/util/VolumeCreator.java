52d336622d7cd21ceb3fa1e41b1c7f7a3f7c7333
Priscila <priscila.campoos@yahoo.com.br>
2024-03-20 16:40:22 +0000

    Implementation of AuthorServiceTest

--- diagonal-library/src/test/java/prisc/diagonallibrary/util/VolumeCreator.java
+++ diagonal-library/src/test/java/prisc/diagonallibrary/util/VolumeCreator.java
@@ -1,7 +1,6 @@
 package prisc.diagonallibrary.util;
 
 import prisc.diagonallibrary.controller.response.VolumeResponse;
-import prisc.diagonallibrary.model.Author;
 import prisc.diagonallibrary.model.Category;
 import prisc.diagonallibrary.model.Volume;
 
@@ -16,7 +15,7 @@ public class VolumeCreator {
                 .title("Harry Potter and the Prisoner of Azkaban")
                 .isbn10("0545582938")
                 .isbn13("9780545582933")
-                .authors(createAuthor())
+                .authors(AuthorCreator.createAuthorSetToSave())
                 .categories(createCategory())
                 .publishedDate("2013-08-27")
                 .language("en")
@@ -50,11 +49,7 @@ public class VolumeCreator {
                 .build();
     }
 
-    private static Set<Author> createAuthor(){
-        return  Set.of(
-                Author.builder().name("J. K. Rowling").build(),
-                Author.builder().name("Kazu Kibuishi").build());
-    }
+
     private static Set<Category> createCategory(){
         return  Set.of(
                 Category.builder().name("Juvenile Fiction").build());
